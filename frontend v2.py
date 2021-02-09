ce8dab0a405fa96fd3913a9d654c9a006ee52a9d
blazingcreeperx <moshidosh83@gmail.com>
2021-02-09 16:04:44 +0000

    fixed window sizing issue

--- frontend v2.py
+++ frontend v2.py
@@ -1,4 +1,15 @@
 import tkinter as tk
+import webview
+
+
+def web_test():
+    webview.create_window(
+        title='Spotify Authentication',
+        url='http://google.com',
+        confirm_close=False
+    )
+    webview.start()
+
 
 base = tk.Tk()  # root window
 
@@ -13,7 +24,10 @@ posy = int(base.winfo_screenheight() / 3 - window_height / 2)
 
 # position the window center of display
 base.geometry('+{}+{}'.format(posx, posy))
+# gives minimum size in px
 base.config(height=500, width=500)
+# disables ability to resize window
+base.resizable(False, False)
 '''
 End
 '''
@@ -30,8 +44,8 @@ choice.set('Artist')  # set the default option
 
 popupMenu = tk.OptionMenu(base, choice, *choices)
 
-tk.Label(base, text="Choose An Option").place(relx=0.5, rely=0.2, anchor='center')
-popupMenu.place(relx=0.5, rely=0.3, anchor='center')
+tk.Label(base, text="Choose An Option").place(relx=0.4, rely=0.2, anchor='center')
+popupMenu.place(relx=0.7, rely=0.2, anchor='center')
 
 
 # on change dropdown value
@@ -42,5 +56,7 @@ def change_dropdown(*args):
 
 # link function to change dropdown
 choice.trace('w', change_dropdown)
+
+tk.Button(base, text='Login to your account', command=lambda: web_test()).place(relx=0.5, rely=0.4, anchor='center')
 
 base.mainloop()
